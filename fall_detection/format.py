31c09af3925a1ba027594f058a09768d7c566645
Jolin Zhang <jolin51502@gmail.com>
2023-12-03 12:20:52 -0800

    model testing

--- fall_detection/format.py
+++ fall_detection/format.py
@@ -13,19 +13,25 @@ def get_x_filename(filename):
     m_codes = ['D01', 'D02', 'D03', 'D04', 'D05', 'D06', 'D07', 'D08', 'D09', 'D10', 'D11', 'D12', 'D13', 'D14', 'D15', 'D16', 'D17', 'D18', 'D19']
     f_codes = ['F01', 'F02', 'F03', 'F04', 'F05', 'F06', 'F07', 'F08', 'F09', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15']
     code = filename.split('_')[0]
+    codes = filename.split('_')
     code_2 = filename.split('_')[1]
     label = ''
-    if code in m_codes or code_2 in m_codes:
-        label = 'ADL'
-    if code in f_codes or code_2 in f_codes:
-        label = 'FALL'
+    # if code in m_codes or code_2 in m_codes:
+    #     label = 'ADL'
+    # if code in f_codes or code_2 in f_codes:
+    #     label = 'FALL'
+    for code in codes:
+        if code == 'ADL':
+            label = 'ADL'
+        if code == 'Fall':
+            label = 'FALL'
     if label == '':
         raise Exception('label not found')
-    x_filename = './data/{}.{}.json'.format(label, os.path.splitext(filename)[0])
+    x_filename = './dataUMA/{}.{}.json'.format(label, os.path.splitext(filename)[0])
     return x_filename
 
 if __name__ == "__main__":
-    files = glob.glob("SisFall/*/*.txt")
+    files = glob.glob("UMAFall/*/*.txt")
     CONVERT_G_TO_MS2 = 9.80665
     for index, path in enumerate(files):
         filename = os.path.basename(path)
@@ -36,9 +42,12 @@ if __name__ == "__main__":
                 if line:
                     row = line
                     cols = row.split(' ')
-                    ax = ((2 * 16) / (2 ** 13)) * float(cols[0]) * CONVERT_G_TO_MS2
-                    ay = ((2 * 16) / (2 ** 13)) * float(cols[1]) * CONVERT_G_TO_MS2
-                    az = ((2 * 16) / (2 ** 13)) * float(cols[2]) * CONVERT_G_TO_MS2
+                    # ax = ((2 * 16) / (2 ** 13)) * float(cols[0]) * CONVERT_G_TO_MS2
+                    # ay = ((2 * 16) / (2 ** 13)) * float(cols[1]) * CONVERT_G_TO_MS2
+                    # az = ((2 * 16) / (2 ** 13)) * float(cols[2]) * CONVERT_G_TO_MS2
+                    ax = float(cols[0]) * CONVERT_G_TO_MS2
+                    ay = float(cols[1]) * CONVERT_G_TO_MS2
+                    az = float(cols[2]) * CONVERT_G_TO_MS2
                     values.append([ax, ay, az])
         if (len(values) == 0):
             continue
