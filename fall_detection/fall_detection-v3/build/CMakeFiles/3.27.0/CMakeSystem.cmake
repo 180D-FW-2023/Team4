cce1941e80cd10e21f9a92f89ec432e37edd78fb
Jolin Zhang <jolin51502@gmail.com>
2023-12-05 15:00:20 -0800

    fall detection initial prototype

---
+++ fall_detection/fall_detection-v3/build/CMakeFiles/3.27.0/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.15.133.1-microsoft-standard-WSL2")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.15.133.1-microsoft-standard-WSL2")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/mnt/c/Users/jolin/OneDrive/Desktop/Senior/fall/EE180DA/Team4/fall_detection/fall_detection-v3/armv6-rpi-linux-gnueabihf.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
