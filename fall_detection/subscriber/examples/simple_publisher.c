742567e8f86ecce177f650334f0f380f6149aca6
Jolin Zhang <jolin51502@gmail.com>
2023-12-05 16:21:38 -0800

    move raspberry pi publisher to pc

--- fall_detection/subscriber/examples/simple_publisher.c
+++ fall_detection/subscriber/examples/simple_publisher.c
@@ -11,6 +11,45 @@
 #include <mqtt.h>
 #include "templates/posix_sockets.h"
 
+// gyro-accel
+#include <sys/time.h>
+#include <stdint.h>
+#include <math.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <time.h>
+#include "../gyro_accelerometer_tutorial01_angles/IMU.c"
+
+// gyro-accel constants
+#define DT 0.02         // [s/loop] loop period. 20ms
+#define AA 0.97         // complementary filter constant
+
+#define A_GAIN 0.0573    // [deg/LSB]
+#define G_GAIN 0.070     // [deg/s/LSB]
+#define RAD_TO_DEG 57.29578
+#define M_PI 3.14159265358979323846
+
+// gyro-accel functions
+void  INThandler(int sig)// Used to do a nice clean exit when Ctrl-C is pressed
+{
+	signal(sig, SIG_IGN);
+	exit(0);
+}
+
+int mymillis()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec) * 1000 + (tv.tv_usec)/1000;
+}
+
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+{
+    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+    result->tv_sec = diff / 1000000;
+    result->tv_usec = diff % 1000000;
+    return (diff<0);
+}
 
 /**
  * @brief The function that would be called whenever a PUBLISH is received.
@@ -43,6 +82,38 @@ int main(int argc, const char *argv[])
     const char* port;
     const char* topic;
 
+    // gyro-accel declarations
+    float rate_gyr_y = 0.0;   // [deg/s]
+	float rate_gyr_x = 0.0;   // [deg/s]
+	float rate_gyr_z = 0.0;   // [deg/s]
+
+	int  accRaw[3];
+	int  magRaw[3];
+	int  gyrRaw[3];
+
+	float acc_x = 0.0;
+	float acc_y = 0.0;
+	float acc_z = 0.0;
+
+	float gyroXangle = 0.0;
+	float gyroYangle = 0.0;
+	float gyroZangle = 0.0;
+	float AccYangle = 0.0;
+	float AccXangle = 0.0;
+	float CFangleX = 0.0;
+	float CFangleY = 0.0;
+
+	int startInt  = mymillis();
+	struct  timeval tvBegin, tvEnd,tvDiff;
+
+    // enable IMU
+    signal(SIGINT, INThandler);
+
+	detectIMU();
+	enableIMU();
+
+	gettimeofday(&tvBegin, NULL);
+
     /* get address (argv[1] if present) */
     if (argc > 1) {
         addr = argv[1];
@@ -71,6 +142,7 @@ int main(int argc, const char *argv[])
         perror("Failed to open socket: ");
         exit_example(EXIT_FAILURE, sockfd, NULL);
     }
+
 
     /* setup a client */
     struct mqtt_client client;
@@ -102,17 +174,45 @@ int main(int argc, const char *argv[])
     printf("%s is ready to begin publishing the time.\n", argv[0]);
     printf("Press ENTER to publish the current time.\n");
     printf("Press CTRL-D (or any other key) to exit.\n\n");
-    while(fgetc(stdin) == '\n') {
+    while(1) {
         /* get the current time */
-        time_t timer;
-        time(&timer);
-        struct tm* tm_info = localtime(&timer);
-        char timebuf[26];
-        strftime(timebuf, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+        // time_t timer;
+        // time(&timer);
+        // struct tm* tm_info = localtime(&timer);
+        // char timebuf[26];
+        // strftime(timebuf, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+
+        startInt = mymillis();
+
+
+		//read ACC and GYR data
+		readACC(accRaw);
+
+		acc_x = (float) ((accRaw[0] * 0.244 / 1000.0) * 9.8);
+		acc_y = (float) ((accRaw[1] * 0.244 / 1000.0) * 9.8);
+		acc_z = (float) ((accRaw[2] * 0.244 / 1000.0) * 9.8);
+
+
+
+		//Change the rotation value of the accelerometer to -/+ 180 and move the Y axis '0' point to up.
+		//Two different pieces of code are used depending on how your IMU is mounted.
+		//If IMU is upside down
+		/*
+			if (AccXangle >180)
+					AccXangle -= (float)360.0;
+
+			AccYangle-=90;
+			if (AccYangle >180)
+					AccYangle -= (float)360.0;
+		*/
+
+
+        char accelbuf[256];
+        snprintf(accelbuf, sizeof(accelbuf), "%7.3f,%7.3f,%7.3f,",acc_x,acc_y,acc_z);
 
         /* print a message */
         char application_message[256];
-        snprintf(application_message, sizeof(application_message), "The time is %s", timebuf);
+        snprintf(application_message, sizeof(application_message), "%s", accelbuf);
         printf("%s published : \"%s\"", argv[0], application_message);
 
         /* publish the time */
@@ -123,6 +223,10 @@ int main(int argc, const char *argv[])
             fprintf(stderr, "error: %s\n", mqtt_error_str(client.error));
             exit_example(EXIT_FAILURE, sockfd, &client_daemon);
         }
+        //Each loop should be at least 20ms.
+		while(mymillis() - startInt < (DT*1000)){
+				usleep(10000);
+		}
     }
 
     /* disconnect */
