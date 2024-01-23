7dd5417c67ad8c66a2e6a6e36cf19de7486b60a8
Jolin Zhang <jolin51502@gmail.com>
2024-01-23 02:01:57 -0800

    continuous fall detection

--- fall_detection/shell_script.sh
+++ fall_detection/shell_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 clear
 echo "running first test"
-./subscriber/bin/simple_subscriber
-wait
-./fall_detection-v3/build/app
-exit 0
+while sleep 10
+{
+    ./subscriber/bin/simple_subscriber
+    wait
+    ./fall_detection-v3/build/app
+}
