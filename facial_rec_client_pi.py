c84d61a9afb4d2bf121fb18c5578003162fc3c86
Krisha <kminocha4@gmail.com>
2024-01-24 16:10:56 -0800

    Update

--- facial_rec_client_pi.py
+++ facial_rec_client_pi.py
@@ -11,6 +11,8 @@ def main():
         return
     # TODO: validate a correct ip address
 
+    print("here2")
+
     # Connect a client socket to my_server:8000 (change my_server to the
     # hostname of your server)
     client_socket = socket.socket()
@@ -51,3 +53,6 @@ def main():
     finally:
         connection.close()
         client_socket.close()
+
+if __name__ == "__main__":
+    main()
