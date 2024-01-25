4d4451e0940a4442ad59058038eb2208e15db9ee
Krisha <kminocha4@gmail.com>
2024-01-24 16:18:39 -0800

    Switching

--- gui/pages/Setup.py
+++ gui/pages/Setup.py
@@ -6,6 +6,6 @@ ssh = ultraimport('__dir__/../../ssh.py', 'main1')
 user = st.text_input('Username')
 pwd = st.text_input('Password')
 hname = st.text_input('Hostname', 'raspberrypi.local')
-pi = st.selectbox('Which Pi?', ('Step Count', 'Facial Recognition', 'Fall Detection'))
+pi = st.selectbox('Which Pi?', ('step_count', 'facial_recognition', 'Fall Detection'))
 
 st.button('Click to Run', on_click = ssh, args = (user, pwd, pi, hname))
