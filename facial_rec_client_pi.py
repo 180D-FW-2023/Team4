7aec8334358eb1cdf7ae92ec60cfc35436afb943
Krisha <kminocha4@gmail.com>
2024-01-29 14:31:51 -0800

    Updated to work with audio

--- facial_rec_client_pi.py
+++ facial_rec_client_pi.py
@@ -1,9 +1,12 @@
+from http import client
 import io
 import socket
 import struct
 import time
 import picamera
+import os
 import sys
+import multiprocessing
 
 def main():
     if len(sys.argv) != 2:
@@ -18,9 +21,11 @@ def main():
     client_socket = socket.socket()
     client_socket.connect((str(sys.argv[1]), 8080))
     client_socket.sendall("face recognition".encode())
-
-    # Make a file-like object out of the connection
     connection = client_socket.makefile('wb')
+
+    p0 = multiprocessing.Process(target=get_name, args=(client_socket,))
+    p0.start()
+
     try:
         camera = picamera.PiCamera()
         camera.resolution = (640, 480)
@@ -43,7 +48,7 @@ def main():
             stream.seek(0)
             connection.write(stream.read())
             # If we've been capturing for more than 30 seconds, quit
-            if time.time() - start > 30:
+            if time.time() - start > 60:
                 break
             # Reset the stream for the next capture
             stream.seek(0)
@@ -53,6 +58,14 @@ def main():
     finally:
         connection.close()
         client_socket.close()
+
+def get_name(client):
+        #Recieve the name message from the server
+        name = client.recv(1024).decode()
+        #print('Received from server: ' + name)  # show in terminal (for now)
+        with open('./nn.txt', 'w') as f:
+            f.write(str(name))
+        #os.system('espeak ' + name + ' 2>/dev/null')
 
 if __name__ == "__main__":
     main()
