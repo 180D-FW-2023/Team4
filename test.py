9f0fe33920ccd85f6fcf7b0e9ba29dfb2c188123
isabellajordan1 <isabella.jordan014@gmail.com>
2024-02-10 15:29:18 -0800

    more error handling

--- test.py
+++ test.py
@@ -1,20 +1,12 @@
-from sense_hat import SenseHat
+import os
+def ping_test(ip):
+    num = 5
+    response = os.popen(f"ping -c {num} {ip} ").read()
+    print(response)
+    count = response.count("Request timeout") + response.count("Request timed out.")
+    print(count)
+    if count >= num-1:
+        return False
+    return True
 
-def main():
-    sense = SenseHat()
-    # Tell the program which function to associate with which direction
-    sense.stick.direction_any = print_num(sense)    # Press the enter key
-
-    while True:
-        pass  # This keeps the program running to receive joystick events
-
-# Define the functions
-def print_num(event, sense):
-    if event.action == 'pressed':
-        sense.show_message('hi')
-
-if __name__ == "__main__":
-    main()
-
-    
-
+print(ping_test("192.168.1.50"))
