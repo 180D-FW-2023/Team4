b45b5712e42301e8eba2c97068ada491e2ad021c
Jolin Zhang <jolin51502@gmail.com>
2024-02-10 14:26:40 -0800

    gps working

--- fall_detection/shell_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-echo "running first test"
-while true
-{
-    ./subscriber/bin/simple_subscriber
-    wait
-    ./fall_detection-v3/build/app
-}
--- fall_detection/shell_script_new.sh
+++ fall_detection/shell_script_new.sh
@@ -1,8 +1,4 @@
 clear
 echo "running first test"
-while true
-do
-    ./subscriber/bin/simple_subscriber
-    wait
-    ./fall_detection-v3/build/app
-done
+./subscriber/bin/gps_subscriber &
+sh while_loop.sh &
