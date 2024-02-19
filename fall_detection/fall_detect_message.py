10427b264bdcd81a4a5f55a016e839b0baf2beaf
Jolin Zhang <jolin51502@gmail.com>
2024-02-18 21:10:21 -0800

    Create fall_detect_message.py

---
+++ fall_detection/fall_detect_message.py
@@ -0,0 +1,39 @@
+import smtplib
+from email.mime.text import MIMEText
+import ssl
+import ast
+
+# create a secure SSL context
+context = ssl.create_default_context()
+
+
+with open('gps.txt', 'r') as f:
+    coords = ast.literal_eval(f.read())
+
+lat = coords["best_lat"]
+lon = coords["best_lon"]
+
+subject = "Fall Detected"
+body = "A fall has been detected at https://www.google.com/maps/search/?api=1&query=" +str(lat) + "," + str(lon)
+sender = "jolin51502@gmail.com"
+recipients = ["notmango60@gmail.com"]
+password = "rkbq epqd bkqz yzqv"
+
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+file = open("fall.txt", "r")
+content = file.readline()
+if content == 'fall\n':
+    send_email(subject, body, sender, recipients, password)
+elif content =="ADL\n":
+    print("Not fall")
+# rkbq epqd bkqz yzqv
