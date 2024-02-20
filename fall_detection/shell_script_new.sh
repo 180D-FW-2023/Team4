2e05156f85a3856b008b702f326a49fa4d43c395
Jolin Zhang <jolin51502@gmail.com>
2024-02-20 12:51:41 -0800

    misspelling

--- fall_detection/shell_script_new.sh
+++ fall_detection/shell_script_new.sh
@@ -1,9 +1,3 @@
 clear
-echo "running first test"
-<<<<<<< HEAD
-./while_loop_gps.sh&
-./while_loop.sh &
-=======
-./fall_detection/while_loop_gps.sh&
+./fall_detection/while_loop_gps.sh &
 ./fall_detection/while_loop.sh &
->>>>>>> 5ef726620f07914bb1c8a57570b7e5898d215ff2
